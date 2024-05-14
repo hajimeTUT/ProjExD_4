329fcfb8a5c152d216e569b7007f36446232deb6
hajime <c011918477@edu.teu.ac.jp>
2024-05-14 14:56:06 +0900

    追加機能４：無敵状態

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -71,6 +71,9 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = xy
         self.speed = 10
+        # 追加4 変数
+        self.state = "normal" # 状態変数
+        self.hyper_life = 0 # 無敵時間
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -98,6 +101,14 @@ class Bird(pg.sprite.Sprite):
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.dire = tuple(sum_mv)
             self.image = self.imgs[self.dire]
+        # 追加4 クラス機能
+        if self.state == "hyper":
+            self.image = pg.transform.laplacian(self.image)
+            self.hyper_life -= 1
+        if self.hyper_life < 0:
+            self.state = "normal"
+            self.image = self.imgs[self.dire]
+
         screen.blit(self.image, self.rect)
 
 
@@ -262,6 +273,11 @@ def main():
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
+            # 追加4
+            if event.type == pg.KEYDOWN and event.key == pg.K_RSHIFT and score.value > 100:
+                bird.state = "hyper"
+                bird.hyper_life = 500
+                score.value -= 100
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
@@ -281,13 +297,19 @@ def main():
             exps.add(Explosion(bomb, 50))  # 爆発エフェクト
             score.value += 1  # 1点アップ
 
-        if len(pg.sprite.spritecollide(bird, bombs, True)) != 0:
-            bird.change_img(8, screen) # こうかとん悲しみエフェクト
-            score.update(screen)
-            pg.display.update()
-            time.sleep(2)
-            return
+        for bomb in pg.sprite.spritecollide(bird, bombs, True):
+            if bird.state == "normal":
+                bird.change_img(8, screen) # こうかとん悲しみエフェクト
+                score.update(screen)
+                pg.display.update()
+                time.sleep(2)
+                return
+            # 追加4
+            elif bird.state == "hyper":
+                exps.add(Explosion(bomb, 50))
+                score.value += 1
 
+    
         bird.update(key_lst, screen)
         beams.update()
         beams.draw(screen)
